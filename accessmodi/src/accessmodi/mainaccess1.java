f2b4c6554af962f2560304e93214dc9bca392256
suraj gurung <suraj gurung>
2015-12-27 15:33:13 -0500

    adding project suraj

---
+++ accessmodi/src/accessmodi/mainaccess1.java
@@ -0,0 +1,10 @@
+package accessmodi;
+
+class mainaccess1 
+{
+	public static int publica=2;
+	private int privatea=3;
+	protected int protecteda=4;
+    int a=5;
+}
+
---
+++ accessmodi/src/accessmodi1/ac1.java
@@ -0,0 +1,9 @@
+package accessmodi1;
+
+public class ac1 {
+	public static void main(String[] args){
+		ac2.v();
+	}
+ 
+	
+} 
