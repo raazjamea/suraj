05bcbed5b60cab8f4d8e4a5a4aa75640aa0702c2
raazjames <raazjames@hotmail.co.uk>
2015-12-28 22:59:08 -0500

    1100

---
+++ accessmodi/src/accessmodi/mainaccess.java
@@ -0,0 +1,20 @@
+package accessmodi;
+
+import accessmodi1.ac2;
+
+public class mainaccess extends mainaccess1 
+{ 
+	public static void main(String[]args){
+	//mainaccess1 t=new mainaccess1();
+	
+	System.out.println(publica);
+	
+	//System.out.println(t.privatea);
+	//System.out.println(t.protecteda);
+	
+	//System.out.println("hello thsi is new add");
+
+	} 
+	
+	
+}
---
+++ accessmodi/src/accessmodi1/ac2.java
@@ -0,0 +1,8 @@
+package accessmodi1;
+
+public class ac2 {
+	static int c=3;
+static void v(){
+System.out.println(c);
+}
+}
